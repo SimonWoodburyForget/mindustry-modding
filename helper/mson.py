7eff4a2f7d7a9d5d83d0eb3ebccc5d96372ccb42
Simon <simonwoodburyforget@gmail.com>
2019-11-09 01:20:07 -0500

    replaced parsy with prasec.py

--- helper/mson.py
+++ helper/mson.py
@@ -5,101 +5,117 @@ the actual Mindustry parser itself, so
 to make parsing other mods possible.
 
 The specific implimentation changes goes as follows:
-- `##` maybe used for single line comments,
+- `//` maybe used for single line comments,
   and also cannot technically be used in strings;
 - `"` quotes aren't required for strings;
 - `,` commas aren't required for arrays or objects.
 """
 
-from parsy import generate, regex, string
 
+'''
+Use parsec.py to parse JSON text.
+'''
 
-whitespace = regex(r'\s*(//.*)?')
-lexeme = lambda p: whitespace >> p << whitespace
+from parsec import *
+
+whitespace = regex(r'\s*')
+comment = string("//") + regex(".*")
+lexeme = lambda p: p << whitespace << comment
+
 lbrace = lexeme(string('{'))
 rbrace = lexeme(string('}'))
 lbrack = lexeme(string('['))
 rbrack = lexeme(string(']'))
-colon  = lexeme(string(':'))
-comma  = lexeme(string(',')).optional() # commas is optional
-true   = lexeme(string('true')).result(True)
-false  = lexeme(string('false')).result(False)
-null   = lexeme(string('null')).result(None)
-number = lexeme(
-    regex(r'-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][+-]?[0-9]+)?')
-).map(float)
-string_part = regex(r'[^"\\]+')
-string_esc = string('\\') >> (
-    string('\\')
-    | string('/')
-    | string('"')
-    | string('b').result('\b')
-    | string('f').result('\f')
-    | string('n').result('\n')
-    | string('r').result('\r')
-    | string('t').result('\t')
-    | regex(r'u[0-9a-fA-F]{4}').map(lambda s: chr(int(s[1:], 16)))
-)
-quoted = lexeme(string('"') >>
-                (string_part | string_esc).many().concat()
-                << string('"'))
-unquoted = lexeme(( regex(r"[a-zA-Z0-9-]")).many().concat()
-                  << regex("[ -,\s\t\n\r]")) # no idea, but it works
+colon = lexeme(string(':'))
+comma = lexeme(string(',')) | lexeme(string('\n'))
+true = lexeme(string('true')).result(True)
+false = lexeme(string('false')).result(False)
+null = lexeme(string('null')).result(None)
 
-# Circular dependency between array and value means we use `generate` form here
+
+
+def number():
+    '''Parse number.'''
+    return lexeme(
+        regex(r'-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][+-]?[0-9]+)?')
+    ).parsecmap(float)
+
+
+def charseq():
+    '''Parse string. (normal string and escaped string)'''
+    def string_part():
+        '''Parse normal string.'''
+        return regex(r'[^"\\]+')
+
+    def string_esc():
+        '''Parse escaped string.'''
+        return string('\\') >> (
+            string('\\')
+            | string('/')
+            | string('"')
+            | string('b').result('\b')
+            | string('f').result('\f')
+            | string('n').result('\n')
+            | string('r').result('\r')
+            | string('t').result('\t')
+            | regex(r'u[0-9a-fA-F]{4}').parsecmap(lambda s: chr(int(s[1:], 16)))
+        )
+    return string_part() | string_esc()
+
+@lexeme
+@generate
+def quoted():
+    '''Parse quoted string.'''
+    yield string('"')
+    body = yield many(charseq())
+    yield string('"')
+    return ''.join(body)
+
+@generate
+def unquoted():
+    '''Parse unquoted string.'''
+    body = yield many(regex(r"[a-zA-Z ]"))
+    return ''.join(body).strip()
+
 @generate
 def array():
+    '''Parse array element in JSON text.'''
     yield lbrack
-    elements = yield value.sep_by(comma)
-    yield comma.optional()
+    elements = yield sepBy(value, comma)
     yield rbrack
     return elements
 
-
 @generate
 def object_pair():
+    '''Parse object pair in JSON.'''
     key = yield quoted | unquoted
     yield colon
     val = yield value
-    yield comma.optional()
     return (key, val)
 
-json_object = lbrace >> object_pair.sep_by(comma).map(dict) << rbrace
-value = quoted | number | json_object | array | true | false | null | unquoted
-json = whitespace >> value
+@generate
+def json_object():
+    '''Parse JSON object.'''
+    yield lbrace
+    pairs = yield sepBy(object_pair, comma)
+    yield rbrace
+    return dict(pairs)
 
-TEST = """
-// Comment 0
-{
-	"name": "Silver Crags", // Comment 4
-	"description": "Salt and silver lie here.",
-	// Comment 5 "loadout": "basicFoundation",
-    "startingItems": [ 
-    	{"item": "copper", "amount": 200},
-    	{"item": "lead", "amount": 300},
-    ],
-// Comment 1
-    "conditi//onWave": 10, // Comment 2
-    "launchPeriod": 10
-    "brequirements": [
-    	{ type : ZoneWave
-    		zone": groundZero
-
-"wave": 40,
-    	}
-    	{
-    		"type": Unlock"
-block": "kiln"
-    	},
-    	{
-    		"type": "Unlock",
-    		"block": "solar-panel"
-    	}
-    ],
-    "resources": ["copper", "silver", "lead", "coal", "sand"]
-}
-
-"""
+value = quoted | number() | json_object | array | true | false | null | unquoted
+jsonc = whitespace >> json_object
 
 if __name__ == '__main__':
-    print(json.parse(TEST))
+    print(jsonc.parse('{"test" : "json"  }'))
+    print(jsonc.parse('{"test" :  json  }'))
+    print(jsonc.parse('{ test  : "json" }'))
+    print(jsonc.parse('{ test  :  json  }'))
+    print(jsonc.parse('''{ test : json,
+                           tist : jons }'''))
+    print(jsonc.parse('''{ test : json 
+                           tist : jons }'''))
+    print(jsonc.parse('''{ test : json 
+//
+                           tist : jons }'''))
+    print(jsonc.parse('''{ test : json 
+//commmmmm
+                           tist : jons }'''))
