380e6deeb64498a8096309351b9806e72b8a4888
Simon <simonwoodburyforget@gmail.com>
2019-12-05 00:16:21 -0500

    Gave page minimum width.

--- helper/style.cljs
+++ helper/style.cljs
@@ -5,23 +5,6 @@
 
 (.log js/console "[init] style.core")
 
-;; ;; Immutable initial state of the page.
-;; (def page (.. js/document -documentElement -outerHTML))
-;; ;; (def page (.serializeToString (new js/XMLSerializer) js/document))
-;; (.log js/console page)
-;; (.log js/console (clj->js (hk/as-hiccup (hk/parse page))))
-
-;; (def toc (-> js/document -document getElementById "table-of-contents"))
-;; (def toc (.getElementById js/document "table-of-contents"))
-
-
-
-;; (def node (.createElement js/document "APP"))
-;; (def textnode (.createTextNode js/document "This is a button."))
-;; (.appendChild node textnode)
-
-;; (.prepend toc node)
-
 (defonce toc (.getElementById js/document "table-of-contents"))
 (defonce pa (.getElementById js/document "postamble"))
 (defonce content (.getElementById js/document "content"))
@@ -29,43 +12,32 @@
 
 (defonce app (.appendChild (.-body js/document)
                             (.createElement js/document "APP")))
-                            ;; ;; (get (.getElementsByClassName toc "text-table-of-contents") 0)
-                            ;; (.-firstChild toc)))
 
-(def menu-state (r/atom "false"))
+(defn stay-on-hash!
+  "hacky way to prevent page from moving on toggle"
+  []
+  (def loc (.-hash (.-location js/window)))
+  (.log js/console (.-hash js/location))
+  (set! (.-hash js/location) loc))
+
+(def menu-state (r/atom false))
 (defn menu-toggle [state]
   (defn unshow []
     (.add (.-classList toc) "unshow")
     (.add (.-classList pa) "unshow")
     (.add (.-classList content) "unshow"))
-
   (defn show []
     (.remove (.-classList toc) "unshow")
     (.remove (.-classList pa) "unshow")
     (.remove (.-classList content) "unshow"))
-  
-  (if (= state "true")
-    (show)
-    (unshow))
-
-  (.log js/console state)
-  
-  ;; (set! (.-(.-style js/console body)))
-  
-  (if (= state "true")
-    "false" "true"))
+  (if state (show) (unshow))
+  (if state false true))
 
 (defn button-component []
-  [:button#toggle {:class (if (= @menu-state "true")
-                            "unshow" "show")
+  [:button#toggle {:class (if @menu-state "unshow" "show")
                    :on-click #(swap! menu-state menu-toggle)} "☰"])
 
 (defn ^:export run []
-  (r/render [button-component]
-            app))
+  (r/render [button-component] app))
 
 (run)
-
-;; (defn ^:export run []
-;;   (r/render [simple-example]
-;;             (js/document.getElementById "app")))
